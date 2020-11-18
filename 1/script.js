8bef26c3bce95ca12b2fd57809f1c209feee5d5f
Warinthon <62070269@it.kmitl.ac.th>
2020-11-18 21:36:37 +0700

    commit on 'master'

--- 1/script.js
+++ 1/script.js
@@ -7,6 +7,33 @@ request.onreadystatechange = function () {
     } }; 
 request.open("GET", requestURL, true); 
 request.send(); 
-function dataReportStatus(data) { 
-    alert(data.tracks.href);
+//data.tracks.items[i].album.artists[0].name
+function dataReportStatus(data) {
+    for (let i = 0; i < data.tracks.items.length; i++) {
+        var item = document.createElement("div");
+        item.setAttribute("class","item");
+
+        var img = document.createElement("img");
+        img.setAttribute("src", data.tracks.items[i].album.images[0].url);
+        img.setAttribute("class","image");
+        item.appendChild(img);
+
+        var name = document.createElement("p");
+        name.innerHTML = "<b>" + data.tracks.items[i].album.name + "<b>";
+        item.appendChild(name);
+
+        var artist = document.createElement("p");
+        artist.innerHTML = "Artist: "+ data.tracks.items[i].album.artists[0].name;
+        item.appendChild(artist);
+        
+        var date = document.createElement("p");
+        date.innerHTML = "Release date: " + data.tracks.items[i].album.release_date;
+        item.appendChild(date);
+
+        var avaliable = document.createElement("p");
+        avaliable.innerHTML = "Avaliable: " + data.tracks.items[i].album.available_markets.length + " countries";
+        item.appendChild(avaliable);
+
+        document.getElementById("container").appendChild(item);
+    }
 }
